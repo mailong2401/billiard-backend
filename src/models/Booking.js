d9ac1641b122f92bb7f001a4a6d444bf70a6da0e
mailong2401 <gtmailong@gmail.com>
2026-04-03 01:01:29 +0700

    update

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -8,7 +8,7 @@ class Booking {
 
     // Generate booking code
     static generateBookingCode() {
-        const date = new Date();
+        const date = new Date() - 7 * 60 * 60 * 1000;;
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
